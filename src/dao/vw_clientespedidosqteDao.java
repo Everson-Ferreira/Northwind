735f8bbc8460d9447a946d1ad66c2e9a5dbe166e
Everson-Fagundes <eversonffagundes@gmail.com>
2020-12-16 00:06:02 -0300

    subindo

---
+++ src/dao/vw_clientespedidosqteDao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.List;
+
+import model.vw_clientespedidosqte;
+
+/**
+ *
+ * @author LENOVO
+ */
+public interface vw_clientespedidosqteDao {
+
+    public vw_clientespedidosqte pesquisarPorIdOrdem(int idordem);
+
+    public List<vw_clientespedidosqte> pesquisarClientesPedidosQte();
+}
